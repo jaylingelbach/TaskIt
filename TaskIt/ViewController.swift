58685cfdaca13a17c59b76321be0e01bd2b8703f
jaylingelbach <jay.lingelbach@gmail.com>
2015-09-01 14:59:33 -0500

    tableviews setup

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -6,10 +6,14 @@
 //  Copyright (c) 2015 FoMo. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // UITableViewDataSource
+    //table view data source requirements
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    //UITableViewDelegate
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
+    }
 }
 
